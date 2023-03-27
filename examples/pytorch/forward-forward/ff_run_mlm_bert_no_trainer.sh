2228cf2150cf46bacd494292af8bdc34aea24a9f
shizukanaskytree <shizukanaskytree@gmail.com>
2023-03-27 03:44:58 -0500

    before modify code to be forward forward algorithm

---
+++ examples/pytorch/forward-forward/ff_run_mlm_bert_no_trainer.sh
@@ -0,0 +1,6 @@
+# (base) wxf@seir19:~/atom_prj/transformers/examples/pytorch/language-modeling$ bash run_mlm_bert_no_trainer.sh
+python ff_run_mlm_no_trainer.py \
+    --dataset_name wikitext \
+    --dataset_config_name wikitext-2-raw-v1 \
+    --model_name_or_path bert-base-uncased \
+    --output_dir /tmp/test-mlm
