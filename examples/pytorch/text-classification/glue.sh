06fd109d3b7925954f5fbf961214ee3da8b7e8ee
shizukanaskytree <shizukanaskytree@gmail.com>
2023-07-21 10:14:37 -0400

    add run script in examples/pytorch/text-classification/run_glue.py

---
+++ examples/pytorch/text-classification/glue.sh
@@ -0,0 +1,16 @@
+export TASK_NAME=mrpc
+export WANDB_MODE=offline
+export NCCL_P2P_DISABLE=1
+# export CUDA_VISIBLE_DEVICES=-1 # -1
+
+python run_glue.py \
+  --model_name_or_path bert-base-cased \
+  --task_name $TASK_NAME \
+  --do_train \
+  --do_eval \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 2 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3 \
+  --output_dir /tmp/$TASK_NAME/ \
+  --overwrite_output_dir
